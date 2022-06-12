20f3fcc375143c8c110273c8fda771281e49f2ca
andretelfer <andretelfer@pop-os.localdomain>
2022-06-12 14:15:47 -0400

    masonry fix

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -9,14 +9,11 @@
 // Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
 
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./masonry.pkgd.min.js"
 
-
-// Option 2
-//
-// Import just what we need
-
-// If you're importing tooltips or popovers, be sure to include our Popper.js dependency
-// import "../../node_modules/popper.js/dist/popper.min.js";
-
-// import "../../node_modules/bootstrap/js/dist/util.js";
-// import "../../node_modules/bootstrap/js/dist/modal.js";
+// Fix masonry glitch for slow loading images
+window.addEventListener("load", ()=>{
+    $('#projects-content').masonry({
+        itemSelector: 'div'
+    })
+})
